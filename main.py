8ced6674534d1c34c49759d34693a939680e9d54
Farhod <zxcvbnmqazwsx287@gmail.com>
2025-10-31 15:30:53 +0500

    Behruz uchun push qilindi))
    😂

---
+++ main.py
@@ -0,0 +1,42 @@
+# #1-masala
+# fruits = ['olma', 'banan', 'uzum']
+
+# fruits.append('nok')
+
+# print(fruits)
+
+# print(len(fruits))
+
+# print('olma' in fruits)
+
+# print(fruits.index('banan'))
+
+
+#2-msala
+# colors = ['qizil', 'yashil', 'ko\'k']
+
+# colors.append('sariq')
+# print(colors)
+
+# colors.sort()
+# print(colors)
+
+# print(len(colors))
+
+# print('yashil' in colors)
+
+
+#3-masala
+cities = ['Toshkent', 'Samarqand', 'Buxoro']
+
+cities.append('Xiva')
+print(cities)
+
+print(cities.index('Samarqand'))
+
+print(len(cities))
+
+cities.sort()
+print(cities)
+
+
